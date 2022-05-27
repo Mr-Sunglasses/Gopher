e15e8dce55a14c633efa4e3fb02e6219036b0989
Kanishk Pachauri <krishna03012003@icloud.com>
2022-05-27 10:01:04 +0000

    learning datatypes

---
+++ datatypes-variables/go.mod
@@ -0,0 +1,3 @@
+module github.com/Mr-Sunglasses/datatypes
+
+go 1.18
---
+++ datatypes-variables/types.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var username string = "Kanishk"
+	var isTrue bool = true
+	var integer uint8 = 255
+	var myvar int = 222
+	fmt.Println(username)
+	fmt.Printf("Variable is of type: %T \n", username)
+}
